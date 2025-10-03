e8a93bf9e3378b42c9d5444f4649471f11df8b7b
Gandhi Balingkit <balingkitgandhi@gmail.com>
2025-10-03 16:47:13 +0800

    homepage commit 1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,38 @@
+import Link from "next/link";
 import React from "react";
 
-import { auth, signOut } from "@/auth";
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log("Session in Home page:", session);
   return (
-    <div>
-      <h2 className="text-2xl font-bold text-primary-500">
-        Welcome to Next JS
-      </h2>
-      {session && (
-        <div className="mt-4">
-          <p className="mb-2">
-            Welcome, {session.user?.name || session.user?.email}!
-          </p>
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <Button type="submit" variant="outline">
-              Sign Out
-            </Button>
-          </form>
-        </div>
-      )}
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
+    </>
   );
 };
 
---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import React, { use, useState } from "react";
+
+interface Props {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      <Image src={imgSrc} alt="Search" width={24} height={24} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+      />
+    </div>
+  );
+};
+
+export default LocalSearch;
