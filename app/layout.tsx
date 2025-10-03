1128ab8315140583372ed278ab9367a8ddccef42
Gandhi Balingkit <balingkitgandhi@gmail.com>
2025-10-03 14:51:20 +0800

    implement sidebars

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 
 import { auth } from "@/auth";
 
 import { Session } from "inspector/promises";
-import { SessionProvider } from "next-auth/react";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -39,6 +39,13 @@ const RootLayout = async ({
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+      </head>
       <SessionProvider session={session}>
         <body
           className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
